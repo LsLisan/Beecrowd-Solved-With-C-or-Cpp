5f5a4c7f83681cb46640ea5150a51c27e80a1e3f
MD. Lisan Islam <135546701+LsLisan@users.noreply.github.com>
2025-08-29 00:03:14 +0600

    Create 1008 - Salary.cpp
    Signed-off-by: MD. Lisan Islam <135546701+LsLisan@users.noreply.github.com>

---
+++ 1008 - Salary.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n , w ;
+    float s,r;
+    scanf ("%d %d %f",&n,&w,&s);
+    r=w*s;
+    printf ("NUMBER = %d\n",n);
+    printf ("SALARY = U$ %.2f\n",r);
+    
+    return 0;
+}
